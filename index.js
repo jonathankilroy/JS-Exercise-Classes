986ebacd413eca0113106ede1edd3699f1fa78b7
Jonathan-Kilroy <jonathan-kilroy@lambdastudents.com>
2020-11-13 01:32:52 -0500

    initial

--- index.js
+++ index.js
@@ -41,8 +41,26 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach =[];
+  }
+  eat(someFood){
+      if(this.stomach.length < 10){
+        this.stomach.push(someFood);
+  }
+}
 
 }
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
 
 /*
   TASK 2
